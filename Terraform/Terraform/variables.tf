e255e2fc9329e4b2baf97bb3eec4dcc3a8e02457
Laith-333 <82454469+Laith-333@users.noreply.github.com>
2026-04-05 20:25:16 +0200

    Add files via upload

---
+++ Terraform/Terraform/variables.tf
@@ -0,0 +1,42 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  default     = "e8fd00c7-068f-4e91-9d44-5e9cdaf82185"
+}
+
+variable "resource_provider_registrations" {
+  description = "Provider registration mode"
+  type        = string
+  default     = "none"
+}
+
+variable "resource_group_name" {
+  default = "SAP_Enveriment_RG"
+}
+
+variable "location" {
+  default = "Italy North"
+}
+
+variable "vnet_name" {
+  default = "SAP_Enveriment_Network"
+}
+
+variable "subnet_name" {
+  default = "Subnet_Secrets"
+}
+
+variable "vnet_address_space" {
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  default = ["10.0.0.0/27"]
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    piplines = "secrets"
+  }
+}
