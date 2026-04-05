6f408b6421e432ee2e9e157d793e8b151f4d2511
Laith-333 <82454469+Laith-333@users.noreply.github.com>
2026-04-05 21:23:47 +0200

    Update variables.tf

--- Terraform/Terraform/variables.tf
+++ Terraform/Terraform/variables.tf
@@ -11,44 +11,31 @@ variable "resource_provider_registrations" {
 }
 
 variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-  default     = "SAP_Enveriment_RG"
+  default = "SAP_Enveriment_RG"
 }
 
 variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "Italy North"
+  default = "Italy North"
 }
 
 variable "vnet_name" {
-  description = "Name of the virtual network"
-  type        = string
-  default     = "SAP_Enveriment_Network"
+  default = "SAP_Enveriment_Network"
 }
 
 variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
-  default     = "Subnet_Secrets"
+  default = "Subnet_Secrets"
 }
 
 variable "vnet_address_space" {
-  description = "Address space for the VNet"
-  type        = list(string)
-  default     = ["10.0.0.0/16"]
+  default = ["10.0.0.0/16"]
 }
 
 variable "subnet_prefix" {
-  description = "Address prefix for the subnet"
-  type        = list(string)
-  default     = ["10.0.0.0/27"]
+  default = ["10.0.0.0/27"]
 }
 
 variable "tags" {
-  description = "Tags to apply to resources"
-  type        = map(string)
+  type = map(string)
   default = {
     piplines = "secrets"
   }
