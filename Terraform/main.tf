c825f8971d74f4b7e7d1e62359ab2bb12aefef46
Laith-333 <82454469+Laith-333@users.noreply.github.com>
2026-04-07 23:00:14 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,27 +1,26 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+}
+
+data "azurerm_client_config" "current" {}
+
+# -------------------------
+# Resource Group (existing)
+# -------------------------
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
 # -------------------------
-# READ existing Key Vault (DATA = read only)
+# Key Vault (existing)
 # -------------------------
 data "azurerm_key_vault" "kv" {
   name                = var.key_vault_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 # -------------------------
-# CREATE secret inside Key Vault
+# Upload Secret
 # -------------------------
 resource "azurerm_key_vault_secret" "config" {
   name         = var.secret_name
