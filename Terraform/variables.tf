31c883690c20f208e4e330c3e1e52bd91cf2fbad
Laith-333 <82454469+Laith-333@users.noreply.github.com>
2026-04-07 17:01:58 +0200

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,16 +7,15 @@ variable "resource_group_name" {
 }
 
 variable "key_vault_name" {
-  description = "Existing Azure Key Vault name"
-  type        = string
+  default = "peiplnessecrets8ie6i"
 }
 
 variable "secret_name" {
-  description = "Name of the secret to store"
+  description = "Name of the secret"
   type        = string
 }
 
 variable "file_path" {
-  description = "Path to file to upload"
+  description = "Path to file"
   type        = string
 }
