a6e23a52bcc0bb8892609d911384c4faacf46050
Laith-333 <82454469+Laith-333@users.noreply.github.com>
2026-04-07 22:43:02 +0200

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,15 +1,17 @@
 variable "subscription_id" {
   description = "Azure Subscription ID"
-  default     = "e8fd00c7-068f-4e91-9d44-5e9cdaf82185"
+  type        = string
 }
 
 variable "resource_group_name" {
   description = "Resource group where Key Vault exists"
+  type        = string
   default     = "SAP_Environment_RG"
 }
 
 variable "key_vault_name" {
   description = "Existing Key Vault name"
+  type        = string
   default     = "peiplnessecrets8ie6i"
 }
 
